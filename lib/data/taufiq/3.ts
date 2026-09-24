422144386809d2ab909460d01b34e9aa6b9d71eb
Taufiq Nurrohman <sossmeed@gmail.com>
2026-09-24 12:46:21 +0700

    202

--- lib/data/taufiq/3.ts
+++ lib/data/taufiq/3.ts
@@ -1,15 +1,20 @@
 import { MusicItem } from '@/lib/musicData';
 
 export const taufiq3: MusicItem[] = [
-    // {
-    //     id: 201,
-    //     judul: "Next Level, aespa",
-    //     link: "https://www.youtube.com/watch?v=v2pt3bDRSPk",
-    //     tahun: "2021 Mei 17",
-    //     playlist : [
-    //         "b",
-    //         "2", "6", "d", "7", "10",
-    //     ],
-    //     added: "31 Mei 2021",
-    // },
+    {
+        id: 202,
+        judul: "happier — Olivia Rodrigo",
+        link: "https://www.youtube.com/watch?v=C_xccaCaQeA",
+        tahun: "21 Mei 2021",
+        playlist : ["b", "3", "1", "4", "7", "60"],
+        added: "21 Februari 2023",
+    },
+    {
+        id: 201,
+        judul: "Sure Thing — Miguel",
+        link: "https://www.youtube.com/watch?v=UfsKdUNTKW0",
+        tahun: "26 November 2010",
+        playlist : ["b", "3", "d", "8", "60"],
+        added: "2 Februari 2023",
+    },
 ];
