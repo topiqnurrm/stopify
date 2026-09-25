3d0139ce143fc952894bb1a4b59faa16fcd37ece
Taufiq Nurrohman <sossmeed@gmail.com>
2026-09-25 16:58:56 +0700

    204

--- lib/data/taufiq/3.ts
+++ lib/data/taufiq/3.ts
@@ -2,6 +2,22 @@ import { MusicItem } from '@/lib/musicData';
 
 export const taufiq3: MusicItem[] = [
     {
+        id: 204,
+        judul: "Saat Kau Telah Mengerti — Virgoun",
+        link: "https://www.youtube.com/watch?v=xXq42vXv02g",
+        tahun: "13 Januari 2023",
+        playlist : ["b", "3", "1", "4", "7", "9"],
+        added: "13 Maret 2023",
+    },
+    {
+        id: 203,
+        judul: "Players — Coi Leray",
+        link: "https://www.youtube.com/watch?v=2pqFcS3VPMU",
+        tahun: "30 November 2022",
+        playlist : ["b", "3", "5", "8", "60"],
+        added: "26 Februari 2023",
+    },
+    {
         id: 202,
         judul: "happier — Olivia Rodrigo",
         link: "https://www.youtube.com/watch?v=C_xccaCaQeA",
